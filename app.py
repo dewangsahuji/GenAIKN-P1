5445bbaa4c7b9764bb6576dc980533a78d05cfda
Dewang Sahu <dewangsahuji@gmail.com>
2025-11-01 00:49:56 +0530

    Few more projects

--- app.py
+++ app.py
@@ -1,37 +1,107 @@
+import streamlit as st
+from pathlib import Path
+from langchain.agents import create_sql_agent
+from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from sqlalchemy import create_engine
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain.agents import AgentExecutor
 import os
 from dotenv import load_dotenv
+load_dotenv(dotenv_path=r"C:\Users\dewan\Coding\GenAIKN\Langchain\.env")
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
+import sqlite3
+from langchain_groq import ChatGroq
 
-from langchain_community.llms import Ollama
-import streamlit as st
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
+st.title("🦜 LangChain: Chat with SQL DB")
 
-load_dotenv()
+LOCALDB="USE_LOCALDB"
+MYSQL="USE_MYSQL"
 
-## Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+radio_opt=["Use SQLLite 3 Database- Student.db","Connect to you MySQL Database"]
 
-## Prompt Template
-prompt=ChatPromptTemplate.from_messages(
-    [
-        ("system","You are a helpful assistant. Please respond to the question asked"),
-        ("user","Question:{question}")
-    ]
+selected_opt=st.sidebar.radio(label="Choose the DB which you want to chat",options=radio_opt)
+
+if radio_opt.index(selected_opt)==1:
+    db_uri=MYSQL
+    mysql_host=st.sidebar.text_input("Provide MySQL Host")
+    mysql_user=st.sidebar.text_input("MYSQL User")
+    mysql_password=st.sidebar.text_input("MYSQL password",type="password")
+    mysql_db=st.sidebar.text_input("MySQL database")
+else:
+    db_uri=LOCALDB
+
+# api_key=st.sidebar.text_input(label="GRoq API Key",type="password")
+
+if not db_uri:
+    st.info("Please enter the database information and uri")
+
+if not api_key:
+    st.info("Please add the groq api key")
+# -------------------------------------
+## LLM model
+# ----------------------
+llm=ChatOpenAI(model_name="gpt-4o",api_key=api_key,streaming=True)
+
+# llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
+
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri==LOCALDB:
+        dbfilepath=(Path(__file__).parent/"student.db").absolute()
+        print(dbfilepath)
+        creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode=ro", uri=True)
+        return SQLDatabase(create_engine("sqlite:///", creator=creator))
+    elif db_uri==MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please provide all MySQL connection details.")
+            st.stop()
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))   
+    
+if db_uri==MYSQL:
+    db=configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+else:
+    db=configure_db(db_uri)
+
+## toolkit
+toolkit=SQLDatabaseToolkit(db=db,llm=llm)
+
+agent=create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+)
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=agent.agent,
+    tools=toolkit.get_tools(),
+    handle_parsing_errors=True,
+    verbose=True
 )
 
-## streamlit framework
-st.title("Langchain Demo With Gemma3 : 1B  Model")
-input_text=st.text_input("What question you have in mind?")
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-## Ollama Llama2 model
-llm=Ollama(model="gemma3:1b")
-output_parser=StrOutputParser()
-chain=prompt|llm|output_parser
+user_query=st.chat_input(placeholder="Ask anything from the database")
 
-if input_text:
-    st.write(chain.invoke({"question":input_text}))
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback=StreamlitCallbackHandler(st.container())
+        response=agent_executor.run(user_query,callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role":"assistant","content":response})
+        st.write(response)
+
+        
 
 
